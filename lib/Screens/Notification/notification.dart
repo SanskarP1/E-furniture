6fa5b21721725f40f981043d400c4ebcc11015e0
SanskarP1 <sanskarpokharel@gmail.com>
2023-06-15 15:16:20 +0545

    sanskar

---
+++ lib/Screens/Notification/notification.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RohiNotify extends StatefulWidget {
+  const RohiNotify({super.key});
+
+  @override
+  State<RohiNotify> createState() => _RohiNotifyState();
+}
+
+class _RohiNotifyState extends State<RohiNotify> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
