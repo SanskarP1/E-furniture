365f5a264155411b69b8734bfb0cd6c97d4ac1f1
SanskarP1 <sanskarpokharel@gmail.com>
2023-06-20 15:04:44 +0545

    modified

--- lib/Utilities/discription.dart
+++ lib/Utilities/discription.dart
@@ -12,10 +12,25 @@ class DescriptionPageState extends State<DescriptionPage> {
   int selectedButtonIndex = 0;
 
   List<Color> buttonColors = [
-    Colors.black,
+    Colors.red,
     Colors.green,
     Colors.brown,
   ];
+  int _count = 1;
+
+  void _incrementCounter() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      if (_count > 1) {
+        _count--;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -85,7 +100,213 @@ class DescriptionPageState extends State<DescriptionPage> {
                       ),
                     ),
                   ),
+                  Positioned(
+                    width: MediaQuery.sizeOf(context).width,
+                    top: 360,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                'A Chair',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  fontFamily: 'Inter',
+                                ),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  // color: Colors.grey,
+                                ),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      decoration: const BoxDecoration(
+                                          borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(20.0),
+                                            bottomLeft: Radius.circular(20.0),
+                                          ),
+                                          color: Color.fromARGB(
+                                              255, 194, 193, 193)),
+                                      child: IconButton(
+                                        splashColor: Colors.transparent,
+                                        highlightColor: Colors.transparent,
+                                        hoverColor: Colors.transparent,
+                                        focusColor: Colors.transparent,
+                                        icon: const Icon(Icons.remove),
+                                        onPressed: _decrementCounter,
+                                      ),
+                                    ),
+                                    Text(
+                                      _count < 10 ? '0$_count' : '$_count',
+                                      style: const TextStyle(fontSize: 24),
+                                    ),
+                                    Container(
+                                      decoration: const BoxDecoration(
+                                          borderRadius: BorderRadius.only(
+                                            topRight: Radius.circular(20.0),
+                                            bottomRight: Radius.circular(20.0),
+                                          ),
+                                          color: Color.fromARGB(
+                                              255, 196, 196, 196)),
+                                      child: IconButton(
+                                        splashColor: Colors.transparent,
+                                        highlightColor: Colors.transparent,
+                                        hoverColor: Colors.transparent,
+                                        focusColor: Colors.transparent,
+                                        icon: const Icon(Icons.add),
+                                        onPressed: _incrementCounter,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(
+                                'Rs.1000',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 20,
+                                  fontFamily: 'Inter',
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+                              Text('(4.0)'),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text('(15 reviews)')
+                            ],
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                            child: Row(
+                              children: [
+                                Text(
+                                  'Description',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                      fontFamily: 'Inter'),
+                                )
+                              ],
+                            ),
+                          ),
+                          const Row(
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('data'),
+                                  Text('data'),
+                                  Text('data'),
+                                  Text('data'),
+                                  Text('data'),
+                                ],
+                              ),
+                            ],
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Row(
+                              children: [
+                                Text('Dimensions:',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold))
+                              ],
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 60),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text('data'),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text('data'),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text('data'),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 10,
+              ),
+              child: SizedBox(
+                height: 60,
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    InkWell(
+                      child: Container(
+                        height: 60,
+                        width: 80,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.black),
+                        child: const Icon(
+                          FontAwesomeIcons.solidHeart,
+                          color: Colors.white,
+                          size: 50,
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      child: Container(
+                          height: 60,
+                          width: 180,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.black),
+                          child: const Text(
+                            'Add to Cart',
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
